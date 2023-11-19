154060cfc2993518da13fc77ca7099eadc330e6d
John Rodrigues <jocimarrodrigues1337@gmail.com>
2023-11-19 14:39:28 -0300

    Iniciando aula 02

---
+++ Aula 02 Compondo componentes/src/App.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <Formulario />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
+
+defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    Formulario
+  }
+})
+</script>
+
+<style scoped></style>
