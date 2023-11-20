4ecb688fd4f2ea62388ff9953fde506d2e09baaf
John Rodrigues <jocimarrodrigues1337@gmail.com>
2023-11-20 10:41:38 -0300

    Refatorando formulário

---
+++ Aula 02 Compondo componentes/src/components/Cronometro.vue
@@ -0,0 +1,36 @@
+<template>
+    <section>
+        <strong>{{ tempoDecorrido }}</strong>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Cronometro',
+    props: {
+        tempoEmSegundos: {
+            type: Number,
+            default: 0 // Valor padrao
+        }
+    },
+    computed: {
+        tempoDecorrido(): string {
+            // return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
+            // Como o método str, foi descontinuado, alterei a lógica do contador, pode causar futuros bugs nas próx aula do curso, mas aí eu resolvo. kk
+
+            const tempoTotal = new Date(this.tempoEmSegundos * 1000);
+            console.log(tempoTotal)
+            const horas = ('0' + tempoTotal.getUTCHours()).slice(-2);
+            const minutos = ('0' + tempoTotal.getUTCMinutes()).slice(-2);
+            const segundos = ('0' + tempoTotal.getUTCSeconds()).slice(-2);
+            console.log(segundos)
+
+            return `${horas}:${minutos}:${segundos}`;
+
+
+        }
+    },
+})
+</script>
--- Aula 02 Compondo componentes/src/components/Formulario.vue
+++ Aula 02 Compondo componentes/src/components/Formulario.vue
@@ -6,9 +6,7 @@
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>{{ tempoDecorrido }}</strong>
-                    </section>
+                    <Cronometro :tempo-em-segundos="tempoEmSegundos"/>
                     <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
@@ -29,29 +27,17 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
+
 export default defineComponent({
     name: 'Formulario',
+    components: {
+        Cronometro
+    },
     data() {
         return {
             tempoEmSegundos: 0,
             cronometro: 0
-        }
-    },
-    computed: {
-        tempoDecorrido(): string {
-            // return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
-            // Como o método str, foi descontinuado, alterei a lógica do contador, pode causar futuros bugs nas próx aula do curso, mas aí eu resolvo. kk
-
-            const tempoTotal = new Date(this.tempoEmSegundos * 1000);
-            console.log(tempoTotal)
-            const horas = ('0' + tempoTotal.getUTCHours()).slice(-2);
-            const minutos = ('0' + tempoTotal.getUTCMinutes()).slice(-2);
-            const segundos = ('0' + tempoTotal.getUTCSeconds()).slice(-2);
-            console.log(segundos)
-
-            return `${horas}:${minutos}:${segundos}`;
-
-
         }
     },
     methods: {
