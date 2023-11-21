6296276eb9a1110bb19152b6c7540fdc1755e6f8
John Rodrigues <jocimarrodrigues1337@gmail.com>
2023-11-21 11:01:01 -0300

    Iniciando aula 04

---
+++ Aula 04 Estilos/src/components/Tarefa.vue
@@ -0,0 +1,34 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{ tarefa.descricao }}
+            </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, Proptype } from 'vue';
+import Cronometro from './Cronometro.vue';
+import type ITarefa from '../interfaces/ITarefa'
+import Box from './Box.vue'
+
+export default defineComponent({
+    name: 'Tarefa',
+    components: {
+        Cronometro,
+        Box
+    },
+    props: {
+        tarefa: {
+            type: Object as Proptype<ITarefa>,
+            required: true
+        }
+    }
+})
+</script>
+
